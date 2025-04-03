133c8a671a89140ba9e99f73cd6d92c92fcd9311
Henrique Costa <contato.henrique19@gmail.com>
2025-04-02 21:44:37 -0300

    itens do cardapio

---
+++ src/pages/Cardapio/Itens/index.tsx
@@ -0,0 +1,13 @@
+import Item from "./Item";
+import styles from "./Itens.module.scss";
+import cardapio from "./itens.json"
+
+export default function Itens() {
+    return (
+        <div className={styles.itens}>
+            {cardapio.map((item) => (
+                <Item key={item.id} {...item}/>
+            ))}
+        </div>
+    )
+}
