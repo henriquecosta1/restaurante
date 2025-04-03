ef2314f20f4e3558b2bd011ec4c1f6ceebf8e747
Henrique Costa <contato.henrique19@gmail.com>
2025-04-02 21:54:17 -0300

    pratos e style background categories

--- src/pages/Cardapio/Itens/Item/index.tsx
+++ src/pages/Cardapio/Itens/Item/index.tsx
@@ -1,6 +1,7 @@
 import styles from "./Item.module.scss";
 import logo from "assets/logo.svg";
 import cardapio from "../itens.json";
+import classNames from "classnames";
 
 type Props = typeof cardapio[0];
 
@@ -17,7 +18,14 @@ export default function Item(props : Props) {
                     <p>{description}</p>
                 </div>
                 <div className={styles.item__tags}>
-                    <div className={styles.item__tipo}>{category.label}</div>
+                    <div 
+                        className={classNames({
+                            [styles.item__tipo]: true,
+                            [styles[`item__tipo__${category.label.toLocaleLowerCase()}`]]: true,
+                        })}
+                    >
+                        {category.label}
+                    </div>
                     <div className={styles.item__porcao}>{size}g</div>
                     <div className={styles.item__qtdpessoas}>Serve {serving} pessoa{serving === 1 ? "" : "s"}</div>
                     <div className={styles.item__valor}>R$ {price.toFixed(2)}</div>
