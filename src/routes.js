494113d6036f6e159f87aa910e99049efa550819
Henrique Costa <contato.henrique19@gmail.com>
2025-04-03 22:08:04 -0300

    rotas principal e e rota relativa

--- src/routes.js
+++ src/routes.js
@@ -1,14 +1,22 @@
 import Cardapio from "pages/Cardapio";
+import Menu from "./components/Menu";
 import Inicio from "pages/Inicio";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import PaginaPadrao from "components/PaginaPadrao";
 
 export default function AppRouter() {
     return (
-        <Router>
-            <Routes>
-                <Route path="/" element={<Inicio />} />
-                <Route path="/cardapio" element={<Cardapio />} />
-            </Routes>
-        </Router>
+        <main>
+            <Router>
+                <Menu />
+                <Routes>
+                    <Route path="/" element={<PaginaPadrao />} >
+                        <Route index element={<Inicio />} />
+                        <Route path="cardapio" element={<Cardapio />} />
+                    </Route>
+                </Routes>
+            </Router>
+
+        </main>
     );
 }
