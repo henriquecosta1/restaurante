07944b2190e078090bd5ba747b2732ba9b65f631
Henrique Costa <contato.henrique19@gmail.com>
2025-04-06 15:34:41 -0300

    not found, footer e botao voltar not found

--- src/routes.js
+++ src/routes.js
@@ -4,10 +4,12 @@ import Inicio from "pages/Inicio";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import PaginaPadrao from "components/PaginaPadrao";
 import Sobre from "pages/Sobre";
+import NotFound from "pages/NotFound";
+import Footer from "./components/Footer";
 
 export default function AppRouter() {
     return (
-        <main>
+        <main className="container">
             <Router>
                 <Menu />
                 <Routes>
@@ -16,7 +18,9 @@ export default function AppRouter() {
                         <Route path="cardapio" element={<Cardapio />} />
                         <Route path="sobre" element={<Sobre />} />
                     </Route>
+                    <Route path="*" element={<NotFound />} />
                 </Routes>
+                <Footer />
             </Router>
 
         </main>
